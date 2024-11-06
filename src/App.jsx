34773fa6526bbb8fe1acf71205e0f134e7c27ab8
Freddy Bijanja <freddybijanja31@gmail.com>
2024-11-06 17:54:56 +0200

    add blog-app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App (){
+  return(
+  <div className="bg-blue-500 w-1/2 mx-auto px-10">
+   <h1 className="text-center text-yellow-100 text-2xl mb-6">Blog</h1>
+   <p className="text-gray-900 hover:text-yellow-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quam porro tempora? Recusandae repudiandae nostrum dolores maiores libero impedit dignissimos, at repellendus, aspernatur nihil ratione sequi ipsa itaque tenetur adipisci in incidunt qui aliquam labore error iure similique voluptas sed. Excepturi ipsam, ab unde cumque iure quas itaque maxime sint dicta deleniti. Pariatur molestias dicta consequuntur, nisi quidem voluptate aut unde, eos reiciendis iure perspiciatis enim quo excepturi laborum. Ipsum fugiat iusto odio. Exercitationem magnam adipisci repellat saepe dicta, modi reprehenderit, enim culpa, aut doloribus omnis ducimus. Delectus, suscipit. Repellendus animi reprehenderit repellat earum temporibus aut natus quaerat veritatis officia, vitae deserunt esse molestias, debitis accusantium nam, eligendi nulla magni voluptatum sequi praesentium explicabo eius. Dignissimos eveniet maxime quia magnam mollitia minima commodi sunt distinctio aut recusandae deserunt nobis corrupti sint sapiente laborum deleniti, temporibus dolores ullam. Possimus architecto itaque cupiditate, laborum tempore voluptas molestias soluta cumque velit ad quibusdam eius perferendis corrupti nisi nostrum sequi asperiores suscipit minus! Ex beatae saepe corporis minus ipsam cumque inventore provident expedita modi! Perspiciatis, ad? Rem voluptatum eveniet culpa vel fugit eum nulla deserunt placeat, quia ad tempora esse ea fuga suscipit pariatur ducimus soluta debitis totam nesciunt incidunt dolorum voluptate, ab quisquam!</p>;
+  </div>
+  )
+}
+
+export default App;
